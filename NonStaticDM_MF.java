f668c592ee6bc19e48f1ec4f764a07a4a1c1b819
Dharamchand <dharamchandpatle917@gmail.com>
2024-12-24 17:04:58 +0530

    update

---
+++ NonStaticDM_MF.java
@@ -0,0 +1,38 @@
+public class NonStaticDM_MF {
+    int a = 10 ;
+    void show(){
+        // Direct calling 
+        System.out.println("THis is non static Data member  "+a);
+        System.out.println("THis is non static member fuction ");
+        
+    }
+
+    NonStaticDM_MF(){
+        
+        System.out.println(a);
+        show();
+    }
+
+    // direct calling 
+    void disp(){
+       
+        show();
+    }
+
+    public static void main(String aa[]){
+
+        // by object
+        System.out.println(new NonStaticDM_MF().a);
+        new NonStaticDM_MF();
+
+        // By reference 
+        NonStaticDM_MF m = new NonStaticDM_MF();
+        m.show();
+        System.out.println(m.a);
+        m.disp();
+
+
+
+
+    }
+}
---
+++ StaticDM_MF.java
@@ -0,0 +1,48 @@
+public class StaticDM_MF {
+    static int a = 10;
+
+    // int a = 10;
+
+    static void dharam() {
+
+        // Agar se mera data member non static hai or mai usko static member function me
+        // call krunga to error aayegi .
+        // System.out.println(a);
+
+        System.out.println(a);
+        System.out.println("Hello ji this is a static member function ");
+        System.out.println("this is a static data  mamber ");
+
+    }
+
+    StaticDM_MF() {
+        System.out.println(" static member function " + a);
+        dharam();
+    }
+// Direct calling 
+    void ram() {
+        dharam();
+    }
+
+    public static void main(String aa[]) {
+        // object
+        System.out.println(new StaticDM_MF().a);
+        new StaticDM_MF();
+
+        // direct
+        System.out.println(a);
+        dharam();
+
+        // by class
+        System.out.println(StaticDM_MF.a);
+        StaticDM_MF.dharam();
+
+        // bye reference 
+        StaticDM_MF m = new StaticDM_MF();
+        m.ram();
+        m.dharam();
+
+
+
+    }
+}
