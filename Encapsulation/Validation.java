d2a34e2ecc4968ead908b6bbb6334407ee85c717
Dharamchand <dharamchandpatle917@gmail.com>
2025-02-06 23:34:32 +0530

    iupdate

--- Encapsulation/Validation.java
+++ Encapsulation/Validation.java
@@ -3,8 +3,8 @@ package Encapsulation;
 public class Validation {
     
     public static String validate (Login login){
-        if(login.getName.equals("admin") && login.getPass.equals("admin")){
-            return "Valid";
+        if(login.getName().equals("admin") && login.getPass().equals("admin")){
+            return "valid";
         }
         else
         return "Invalid";
